ecc17ed4c7e7e283befa1be94bc73c2d0d1f1e6d
Blue Notebook <hosseinmohammadkhani8302@gmail.com>
2026-01-06 18:38:56 +0330

    2D discrete convolution (Image blurring) added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import io
 import streamlit as st
 import matplotlib.pyplot as plt
+from PIL import Image
 
-from helpers import convolve, apply_gaussian_filter, parse_signal
+import helpers as hp
 
 # Webpage configuration
-st.set_page_config(page_title="کانولوشن یک بعدی", layout="centered")
+st.set_page_config(page_title="کانولوشن دو بعدی", layout="centered")
 
 st.markdown("""
     <style>
@@ -21,11 +23,11 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("کانولوشن گسسته یک بعدی")
+st.title("کانولوشن گسسته")
 
 option = st.selectbox(
     "نوع عملیات را انتخاب کنید: ",
-    ("کانولوشن ساده", "اعمال فیلتر گاوسی")
+    ("کانولوشن ساده", "اعمال فیلتر گاوسی", "اعمال فیلتر گاوسی روی تصویر")
 )
 
 # initialize result signal
@@ -39,10 +41,10 @@ if option == "کانولوشن ساده":
         input2 = st.text_input("سیگنال دوم (اعداد را با کاما جدا کنید)")
     
     if st.button("محاسبه"):
-        s1 = parse_signal(input1)
-        s2 = parse_signal(input2)
+        s1 = hp.parse_signal(input1)
+        s2 = hp.parse_signal(input2)
         if s1 and s2:
-            result_signal = convolve(s1, s2)
+            result_signal = hp.convolve(s1, s2)
         else:
             st.error("لطفاً سیگنال‌ها را به درستی وارد کنید (مثال: 1, 2, 3)")
 
@@ -51,13 +53,46 @@ elif option == "اعمال فیلتر گاوسی":
     sigma_val = st.number_input("مقدار سیگما" , 1 , None, 1)
     
     if st.button("محاسبه"):
-        parsed_signal = parse_signal(input_signal)
+        parsed_signal = hp.parse_signal(input_signal)
         if parsed_signal:
-            result_signal = apply_gaussian_filter(parsed_signal, sigma=sigma_val)
+            result_signal = hp.apply_1d_gaussian_filter(parsed_signal, sigma=sigma_val)
         else:
             st.error("لطفاً سیگنال را به درستی وارد کنید.")
 
-# show output
+elif option == "اعمال فیلتر گاوسی روی تصویر":
+    st.write("لطفاً یک تصویر آپلود کنید. تصویر به صورت خاکستری شده و به اندازه 512×512 تغییر اندازه می‌یابد.")
+    input_image = st.file_uploader("انتخاب تصویر (jpg/png)", type=["jpg", "jpeg", "png"])
+    sigma_img = st.number_input("مقدار سیگما برای فیلتر گاوسی تصویر", min_value=0.1, value=1.0, step=0.1, format="%.2f")
+    
+    if input_image is not None:
+        st.image(input_image, caption="تصویر آپلود شده (ورودی)")
+        if st.button("تبدیل"):
+            try:
+
+                # convert input image to 2D matrix
+                img_matrix = hp.convert_image_to_matrix(input_image)
+                
+                # apply 2D gaussian filter (returns blurred matrix)
+                blurred_matrix = hp.apply_gaussian_filter_on_image(img_matrix, sigma=sigma_img)
+                
+                buf = io.BytesIO()
+                hp.matrix_to_image(blurred_matrix , buf)
+                buf.seek(0)
+                
+                st.image(buf, caption="تصویر تار شده")
+                
+                # provide download button
+                st.download_button(
+                    label="دانلود تصویر تار شده",
+                    data=buf.getvalue(),
+                    file_name="blurred_image.png",
+                    mime="image/png"
+                )
+                
+            except Exception as e:
+                st.error(f"خطا هنگام پردازش تصویر: {e}")    
+
+# show output(for 1D convolution)
 if result_signal:
     st.subheader("خروجی نهایی")
     
--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import math
+from PIL import Image
 
 def convolve(x , h):
 
@@ -21,7 +22,7 @@ def convolve(x , h):
 
     return output_signal
 
-def apply_gaussian_filter(signal , sigma):
+def apply_1d_gaussian_filter(signal , sigma):
 
     # Radius determines how far from the center Gaussian samples are kept
     # Keeping values within ±3σ keeps 99.7% of the Gaussian’s total energy(mass)
@@ -43,3 +44,93 @@ def parse_signal(input_signal):
         return [float(x.strip()) for x in input_signal.split(',')]
     except:
         return None
+
+def convolve2d(input_signal, kernel):
+    H = len(input_signal)
+    W = len(input_signal[0])
+
+    Kh = len(kernel)
+    Kw = len(kernel[0])
+
+    # Output size of full convolution
+    y_h = H + Kh - 1
+    y_w = W + Kw - 1
+
+    # Initialize output with zeros
+    y = [
+        [0.0 for _ in range(y_w)]
+        for _ in range(y_h)
+    ]
+
+    for p in range(y_h):
+        for q in range(y_w):
+            acc = 0.0
+
+            for i in range(Kh):
+                for j in range(Kw):
+                    x_row = p - i
+                    x_col = q - j
+
+                    if 0 <= x_row < H and 0 <= x_col < W:
+                        acc += input_signal[x_row][x_col] * kernel[i][j]
+
+            y[p][q] = acc
+
+    return y
+
+def convert_image_to_matrix(path):
+
+    # Load image
+    img = Image.open(path)
+
+    # Grayscale
+    gray_img = img.convert("L")
+
+    # Resize to 512x512
+    resized_img = gray_img.resize((512, 512))
+
+    # Convert to 2D matrix
+    image_matrix = list(resized_img.getdata())
+    image_matrix = [
+        image_matrix[i * 512:(i + 1) * 512]
+        for i in range(512)
+    ]
+
+    return image_matrix
+
+
+def apply_gaussian_filter_on_image(input_signal , sigma=1.0):
+    radius = 2
+    kernel = []
+    total = 0.0
+
+    for i in range(-radius, radius + 1):
+        row = []
+        for j in range(-radius, radius + 1):
+            value = math.exp(-(pow(i , 2) + pow(j , 2)) / (2 * pow(sigma , 2)))
+            row.append(value)
+            total += value
+        kernel.append(row)
+
+    # Normalization
+    for i in range(5):
+        for j in range(5):
+            kernel[i][j] /= total
+
+    return convolve2d(input_signal, kernel)
+
+def matrix_to_image(matrix, path):
+    h = len(matrix)
+    w = len(matrix[0])
+
+    img = Image.new("L", (w, h))
+    pixels = []
+
+    for row in matrix:
+        for v in row:
+            v = int(round(v))
+            v = max(0, min(255, v))
+            pixels.append(v)
+
+    img.putdata(pixels)
+    img.save(path , format="PNG")
